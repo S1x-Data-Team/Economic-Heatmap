d71c614a906db6fee168ee2cb896fb1a5f035853
erik <erikpries2997@gmail.com>
2025-03-07 23:30:10 +0100

    Fixed table name

--- public/compare.js
+++ public/compare.js
@@ -1,5 +1,6 @@
 const indicators = ["GDP", "M-PMI", "S-PMI", "RetailSales", "Inflation", "Unemployment",];
-
+let forecastScore1 = 0;
+let forecastScore2 = 0;
 async function fetchIndicator(region, indicator) {
     try {
         const response = await fetch(`/data/${region}/${indicator}.json`);
@@ -21,41 +22,30 @@ async function compareRegions(region1, region2, tableId, scoreId, pairName) {
     tbody.innerHTML = "";
 
     for (let indicator of indicators) {
+        if(indicator == "Unemployment") {
         const data1 = await fetchIndicator(region1, indicator);
         const data2 = await fetchIndicator(region2, indicator);
 
         if (data1.actual === null || data2.actual === null) continue;
 
         let strengthScore = 0;
-        /*         if (indicator == "Unemployment") {
-                    if (data1.actual < data2.actual) strengthScore = 1;
-                    else if (data1.actual > data2.actual) strengthScore = -1;
-                }
-                else {
-                    if (data1.actual > data2.actual) strengthScore = 1;
-                    else if (data1.actual < data2.actual) strengthScore = -1;
-                } */
-        let forecastScore1 = 0;
-        let forecastScore2 = 0;
-        if (indicator = "Unemployment") {
-
-            if (data1.actual < data1.forecast) forecastScore1 = 1;
-            else if (data1.actual > data1.forecast) forecastScore1 = -1;
-
-            if (data2.actual < data2.forecast) forecastScore2 = 1;
-            else if (data2.actual > data2.forecast) forecastScore2 = -1;
+/*         if (indicator == "Unemployment") {
+            if (data1.actual < data2.actual) strengthScore = 1;
+            else if (data1.actual > data2.actual) strengthScore = -1;
         }
         else {
+            if (data1.actual > data2.actual) strengthScore = 1;
+            else if (data1.actual < data2.actual) strengthScore = -1;
+        } */
 
-            if (data1.actual > data1.forecast) forecastScore1 = 1;
-            else if (data1.actual < data1.forecast) forecastScore1 = -1;
+        if (data1.actual < data1.forecast) forecastScore1 = 1;
+        else if (data1.actual > data1.forecast) forecastScore1 = -1;
 
-            if (data2.actual > data2.forecast) forecastScore2 = 1;
-            else if (data2.actual < data2.forecast) forecastScore2 = -1;
-        }
+        if (data2.actual < data2.forecast) forecastScore2 = 1;
+        else if (data2.actual > data2.forecast) forecastScore2 = -1;
 
         // Adjust strength score based on forecast beats/misses
-        const adjustedScore = forecastScore1 - forecastScore2;
+        const adjustedScore =forecastScore1 - forecastScore2;
         totalScore += adjustedScore;
 
         // Add row with adjusted score
@@ -63,6 +53,38 @@ async function compareRegions(region1, region2, tableId, scoreId, pairName) {
             <td>${indicator}</td>
             <td style="text-align: center;">${adjustedScore}</td>
         </tr>`;
+        }
+        else{        const data1 = await fetchIndicator(region1, indicator);
+            const data2 = await fetchIndicator(region2, indicator);
+    
+            if (data1.actual === null || data2.actual === null) continue;
+    
+            let strengthScore = 0;
+    /*         if (indicator == "Unemployment") {
+                if (data1.actual < data2.actual) strengthScore = 1;
+                else if (data1.actual > data2.actual) strengthScore = -1;
+            }
+            else {
+                if (data1.actual > data2.actual) strengthScore = 1;
+                else if (data1.actual < data2.actual) strengthScore = -1;
+            } */
+    
+            if (data1.actual > data1.forecast) forecastScore1 = 1;
+            else if (data1.actual < data1.forecast) forecastScore1 = -1;
+    
+            if (data2.actual > data2.forecast) forecastScore2 = 1;
+            else if (data2.actual < data2.forecast) forecastScore2 = -1;
+    
+            // Adjust strength score based on forecast beats/misses
+            const adjustedScore =forecastScore1 - forecastScore2;
+            totalScore += adjustedScore;
+    
+            // Add row with adjusted score
+            tbody.innerHTML += `<tr>
+                <td>${indicator}</td>
+                <td style="text-align: center;">${adjustedScore}</td>
+            </tr>`;
+        }
     }
 
     // Set final score with color
